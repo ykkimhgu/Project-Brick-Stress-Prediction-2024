f550625fc295422e68cd528913ed1697bb1aca09
YangJiWoo7240 <yangjiu2504@gmail.com>
2024-12-17 12:26:28 +0900

    Update config.py

--- config.py
+++ config.py
@@ -10,13 +10,13 @@ current_time = datetime.now().strftime("%Y%m%d_%H%M%S")  # 예: 20240427_153045
 
 # train path
 train_inputExcel_path   = "data/train/input/excel/trainData.xlsx"
-train_inputImage_path   = "data/train/input/image/" 
+train_inputImage_path   = "data/train/input/images/" 
 train_output_path       = f"data/train/output/model/model_{current_time}.joblib"
 
 
 # test path
 test_inputExcel_path   = "data/test/input/excel/testData.xlsx"
-test_inputImage_path   = "data/test/input/image/"
+test_inputImage_path   = "data/test/input/images/"
 test_model_path        = f"data/train/output/model/model_20241212_165536.joblib"
 test_output_path   = "data/test/output/"
 
