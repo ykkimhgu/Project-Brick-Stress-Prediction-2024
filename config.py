170d708477b88b8bedabb3b66fcffa9c60cd088a
YangJiWoo7240 <yangjiu2504@gmail.com>
2024-12-18 14:06:18 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,39 +1,22 @@
-from sklearn.preprocessing import MinMaxScaler
-import matplotlib.pyplot as plt
-import seaborn as sns
 from datetime import datetime
 # -----------------------------------Time config-------------------------------------#
 current_time = datetime.now().strftime("%Y%m%d_%H%M%S")  # 예: 20240427_153045
+
+# -----------------------------------Folder Path-------------------------------------#
+# Mode select : train or test
+mode = "test"
 
 # -----------------------------------Folder Path-------------------------------------#
 
 
 # train path
 train_inputExcel_path   = "data/train/input/excel/trainData.xlsx"
-train_inputImage_path   = "data/train/input/images/" 
+train_inputImage_path   = "data/train/input/image/" 
 train_output_path       = f"data/train/output/model/model_{current_time}.joblib"
 
 
 # test path
 test_inputExcel_path   = "data/test/input/excel/testData.xlsx"
-test_inputImage_path   = "data/test/input/images/"
-test_model_path        = f"data/train/output/model/model_20241212_165536.joblib"
-test_output_path   = "data/test/output/"
-
-
-# -------------------------------------mode select----------------------------------#
-MIM_VALUE = 0
-MAX_VALUE = 0
-
-
-# 데이터 컬럼 이름 정의
-column_names = [
-    "Cement", "Sand", "Crushed Gravel", "Water", "Foaming Agent", "Water-Reducing Agent", 
-    "Fly Ash", "Silica Fume", "Ground Granulated Blast-Furnace Slag (GGBFS)", "Fiber", 
-    "Weight", "Void Pixel Ratio", "Mean", "Variance", "Standard Deviation", 
-    "Skewness", "Kurtosis", "Entropy", "Contrast", "Energy", "Uniformity", "Correlation"
-]
-
-
-# Material Types
-material_types = ["WR", "BFS", "Orin", "W", "F", "deF", "SF", "gSF", "FA"]
+test_inputImage_path   = "data/test/input/image/"
+test_model_path        = f"data/train/output/model/model_20241217_164408.joblib"
+test_output_path       = f"data/test/output/{current_time}_predictResult.xlsx"
